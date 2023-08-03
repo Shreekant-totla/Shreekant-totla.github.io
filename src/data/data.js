23a1bca1080d0ccc1e34811259796b323bb9d83e
Shreekant-totla <shreekanttotla3@gmail.com>
2023-08-03 19:34:17 +0530

    changes done

--- src/data/data.js
+++ src/data/data.js
@@ -1,24 +1,18 @@
-import WorkIm from "../assets/projects/workImg.jpeg";
-import RealEst from "../assets/projects/realestate.jpg";
-import Snapdeal from "../assets/projects/snapdeal.png";
-import Nordstorm from "../assets/projects/nordstorm.png";
-import GithubRepoSearch from "../assets/projects/gitRepoSearch.png";
-import promiseImg from "../assets/mobile.png";
-import thisImg from "../assets/creator.png";
-import useReducerImg from "../assets/web.png";
-import useStateImg from "../assets/backend.png";
-import CodeVortex from "../assets/CodeVortexInterview.png";
+import CreditGuru from "../assets/projects/creditguru.png";
+import MusicApp from "../assets/projects/musicapp.png";
+import SellOutBazzar from "../assets/projects/selloutbazzar.png";
+import Trade24 from "../assets/projects/trade24.png";
 
 export const data = [
   {
     id: 1,
     name: "React JS Application",
-    pName: "Flashdeal",
+    pName: "CreditGuru",
     pDesc:
-      "Flashdeal is a clone of Snapdeal. Snapdeal is an Indian e-commerce company that offers a wide range of clothing, accessories, and footwear.",
-    image: Snapdeal,
-    github: "https://github.com/GRAviTY-GAGAN/-invincible-ice-6073",
-    live: "https://grand-dasik-526567.netlify.app/",
+      "Welcome to Credit Guru, an online platform that offers users the ability to apply for various types of loans and credit cards. This README file provides an overview of the project, its features, and instructions for setting up and running the website locally.",
+    image: CreditGuru,
+    github: "https://github.com/Shreekant-totla/credit-guru",
+    live: "credit-guru.vercel.app/",
     tech: [
       { name: "#ReactJS", color: "red" },
       { name: "#ChakraUi", color: "orange" },
@@ -27,56 +21,49 @@ export const data = [
   },
   {
     id: 2,
-    name: "JavaScript Application",
-    image: Nordstorm,
-    pName: "Sandstorm",
+    name: "React JS Application",
+    image: Trade24,
+    pName: "Trade24",
     pDesc:
-      "Sandstorm is a clone of Nordstorm. Nordstrom is a high-end American department store chain that sells clothing, shoes, accessories, and beauty products for women, men, and children.",
-    github: "https://github.com/GRAviTY-GAGAN/righteous-hand-1433",
-    live: "https://sandstorm-gsvr.netlify.app/",
+      "This clone is an online marketplace for reviewing and buying stocks , and can tally stocks according to 52 weeks high and low data.",
+    github: "https://github.com/Shreekant-totla/trade-24",
+    live: "trade24.vercel.app/",
+    tech: [
+      { name: "#ReactJS", color: "red" },
+      { name: "#MongoDB", color: "orange" },
+      { name: "#ExpressJS", color: "green" },
+    ],
+  },
+  {
+    id: 3,
+    name: "Next JS Application",
+    image: MusicApp,
+    pName: "SavvanMusic",
+    pDesc:
+      "This is an online platform for listening and streaming music",
+    github: "https://github.com/Shreekant-totla/Music-app",
+    live: "music-app-123.vercel.app/",
+    tech: [
+      { name: "#NextJS", color: "red" },
+      { name: "#Tailwind", color: "orange" },
+      { name: "#JavaScript", color: "green" },
+    ],
+  },
+  {
+    id: 4,
+    name: "JavaScript Application",
+    image: SellOutBazzar,
+    pName: "SellOutBazzar",
+    pDesc:
+      "This clone is an online marketplace for selling and buying refurbished products in various categories like electronics, wardrobes etc.",
+    github: "https://github.com/Shreekant-totla/Sell-Out-Bazzar",
+    live: "ubiquitous-fenglisu-8fb234.netlify.app/",
     tech: [
       { name: "#JavaScript", color: "red" },
       { name: "#CSS", color: "orange" },
       { name: "#json-server", color: "green" },
     ],
-  },
-  {
-    id: 3,
-    name: "React JS Application",
-    image: GithubRepoSearch,
-    pName: "Github Repository Search App",
-    pDesc:
-      "A app using which you can search for any user on github. you will get to see the details of the user and also the repositories of the user.",
-    github: "https://github.com/GRAviTY-GAGAN/Github-Repositiry-Search-App",
-    live: "https://github-repository-search1.netlify.app",
-    tech: [
-      { name: "#ReactJS", color: "red" },
-      { name: "#CSS", color: "orange" },
-      { name: "#Firebase", color: "green" },
-    ],
-  },
-  {
-    id: 4,
-    name: "JS Application",
-    image: loud-weight-1875,
-    pName: "Sell Out Bazzar",
-    pDesc:
-      "Sell Out Bazzar is an online marketplace for selling and buying refurbished products in various categories like electronics, wardrobes etc.",
-    github: "https://github.com/Shreekant-totla/Sell-Out-Bazzar",
-    live: "https://ubiquitous-fenglisu-8fb234.netlify.app/",
-    tech: [
-      { name: "#JavaScript", color: "red" },
-      { name: "#HTML", color: "orange" },
-      { name: "#CSS", color: "green" },
-    ],
-  },
-  //   {
-  //     id: 4,
-  //     name: "React JS Application",
-  //     image: RealEst,
-  //     github: "",
-  //     live: "",
-  //   },
+  }
 ];
 
 
